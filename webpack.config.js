3b8e83620306acdf47b114802e9e5865ef084282
Moody Salem <moody.salem@gmail.com>
2018-10-14 02:02:10 -0400

    also bundle with webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts']
+  },
+  output: {
+    filename: 'jointz.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'jointz',
+    libraryExport: 'default',
+    libraryTarget: 'umd'
+  }
+};
