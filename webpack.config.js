b006fdbf5286b412c86cecf6279b43eb00dbb29d
moody <moody.salem@gmail.com>
2018-10-15 20:17:41 -0400

    externalize source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.ts',
   mode: 'production',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   module: {
     rules: [
       {
